95ece906db3aeceff9b7fb10212bf5dec6208e12
Your GitHub Username <sumeyahassensumi@gmail.com>
2025-07-30 20:02:56 -0700

    complated reponsiveness and  add event

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+var menu=document.getElementById("manu");
+var navigation=document.getElementById("navigation-bar");
+menu.addEventListener("click",function(){
+    navigation.classList.toggle("active");
+    
+
+})
